7d8eb5983320977a417737ec63f5befb19f189c3
kmkathir <kmkathirr10@gmail.com>
2025-12-30 23:17:37 +0530

    c code done

--- Room_rent_qsn.c
+++ Room_rent_qsn.c
@@ -4,4 +4,15 @@ int main()
 {
     int nor,month,r;
     scanf("%d %d",&nor ,&month);
-   
+    if (month == 12 || month == 11 || month == 1 || month == 4)
+    {
+        r=nor*1300 ;
+    }
+    else 
+    {
+        r=nor*1000 ;
+    }
+    printf("room rent = %d",r);
+    
+    return 0;
+}
