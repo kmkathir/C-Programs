009f2f668317c90d1422179bcd52be25594d3d06
Kathiresan M <kmkathirr10@gmail.com>
2026-01-21 17:39:21 +0530

    Changes Made

--- Array_Sorting_Descending.c
+++ Array_Sorting_Descending.c
@@ -2,14 +2,14 @@
 int main()
 {
   int a[10],i,n,j,temp;
-  printf("Enter number of elements : ");
+  printf("Enter number of elements :\n ");
   scanf("%d",&n);
   printf("Enter Array elements : ");
   for(i=0;i<n;i++){
     scanf("%d",&a[i]);
   }
 for(i=0;i<n-1;i++){
-  for(j=n+1;j<n;j++){
+  for(j=i+1;j<n;j++){
     if(a[i]<a[j]){
       temp = a[i];
       a[i] = a[j];
