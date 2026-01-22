ea469d2fabdbd6d549d38665f035196d27795390
Kathiresan M <kmkathirr10@gmail.com>
2026-01-22 22:53:51 +0530

    Update Find_Majority_Element.c

--- Find_Majority_Element.c
+++ Find_Majority_Element.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],size,n,i,j,count;
+    int majority = -1;
+    printf("Enter array size : ");
+    scanf("%d",&size);
+    
+    printf("Enter array elements : ");
+    for(i=0;i<size;i++)
+    scanf("%d",&a[i]);
 
+  
+  
+  
+  
+  
+  
+  
+  
+  return 0;   
+}
