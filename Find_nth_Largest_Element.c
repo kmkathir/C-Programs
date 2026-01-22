4a013a317f3b5b3a7e1e0ead22def6d11d9fba33
Kathiresan M <kmkathirr10@gmail.com>
2026-01-22 22:18:27 +0530

    Refactor Find_nth_Largest_Element.c for clarity
    Updated the array size to 10 and modified prompts for user input.

--- Find_nth_Largest_Element.c
+++ Find_nth_Largest_Element.c
@@ -1,30 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int a[100],size,i,n,j,temp;
-
-	printf("Enter size of array :\n");
-	scanf("%d",&size);
-
-	printf("Enter array elements :\n");
-	for(i=0; i<size; i++)
-		scanf("%d",&a[i]);
-
-	printf("Enter n :");
-	scanf("%d",&n);
-
-	for(i=0; i<size; i++) {
-		for(j=i+1; j<size; j++) {
-			if(a[i] < a[j] ) {
-				temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
-			}
-		}
-	}
-	if(n>size)
-		printf("no nth largest number");
-	else
-		printf("%dth largest element is : %d",n,a[n-1]);
-	return 0;
-}
+    int a[10],size,i,j,n,temp;
+    printf("Enter array size :");
+    scanf("%d",&size);
+    
+    printf("Enter array elements :");
+    for(i=0;i<size;i++)
+    scanf("%d",&a[i]);
+    
+    printf("n value :");
+    scanf("%d",&n);
+    
+    for(i=0;i<size-1;i++){
+        for(j=i+1;j<size;j++){
+            if( a[i] < a[j] )
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                
+            }
+        }
+    }
