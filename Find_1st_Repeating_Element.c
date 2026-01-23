b1f9a9d6c3b4a1bf7e7f5d27e508bcfdd594f1bc
Kathiresan M <kmkathirr10@gmail.com>
2026-01-23 13:22:40 +0530

    Implement first repeating element detection
    Added logic to find the first repeating element in an array.

--- Find_1st_Repeating_Element.c
+++ Find_1st_Repeating_Element.c
@@ -9,7 +9,23 @@ int main()
   printf("Enter array elements : ");
   for(i=0;i<size;i++)
     scanf("%d",&a[i]);
-  
+
+ for (i=0;i<size;i++)
+    {
+      for(j=i+1;j<size;j++)
+        {
+          if( a[i] ==a[j] )
+          {
+          printf("First Repeating element is : %d",a[i]);
+          found =1;
+          break;
+        }
+        }
+    if(found)
+      break;
+    }
+  if(!found)
+    printf("No repeating elemnt found :");
   
 
   return 0;
