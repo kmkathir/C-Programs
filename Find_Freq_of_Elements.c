05858221c651add90a513f6cc83767965424c14b
Kathiresan M <kmkathirr10@gmail.com>
2026-01-23 12:51:24 +0530

    Update and rename Find_Freq_of_Elements to Find_Freq_of_Elements.c

---
+++ Find_Freq_of_Elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a[10],i,j,size,count;
+    
+  printf("Enter srray size :");
+  scanf("%d",&size);
+  
+  printf("Enter array elements :");
+    for(i=0;i<size;i++)
+    scanf("%d",&a[i]);
+    
+    printf("\nElement Frequency\n");
+
+
+  return 0;
+}
