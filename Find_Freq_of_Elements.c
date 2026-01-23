0ab0c4401800b5f619cf8e5543d5f2d9e15d4d95
Kathiresan M <kmkathirr10@gmail.com>
2026-01-23 12:51:46 +0530

    Implement frequency count for array elements
    Added logic to calculate and print the frequency of each element in the array.

--- Find_Freq_of_Elements.c
+++ Find_Freq_of_Elements.c
@@ -11,7 +11,27 @@ int main()
     scanf("%d",&a[i]);
     
     printf("\nElement Frequency\n");
+  
+  for(i=0;i<size;i++)
+    {
+      count = 1;
 
+      for(j=0;j<i;j++)
+        {
+          if(a[i] == a[j])
+            break;
+        }
+      if(j!=i)
+        continue;
+
+      for(j=i+1;j<size;j++)
+        {
+          if( a[i] == a[j] )
+            count++;
+        }
+
+  printf("%d\t\t%d\n",a[i],count);
+    }
 
   return 0;
 }
