2419b3b22fedd3d11c919cdfe9959df02ae76b03
Kathiresan M <kmkathirr10@gmail.com>
2026-01-23 13:22:21 +0530

    Update Find_1st_Repeating_Element.c

--- Find_1st_Repeating_Element.c
+++ Find_1st_Repeating_Element.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int a[10],i,j,size,count=0,found;
 
+  printf("Enter array size: ");
+  scanf("%d",&size);
+
+  printf("Enter array elements : ");
+  for(i=0;i<size;i++)
+    scanf("%d",&a[i]);
+  
+  
+
+  return 0;
+
+}
