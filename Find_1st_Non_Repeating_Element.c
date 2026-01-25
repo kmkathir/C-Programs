4caf5a363ebd72c3f262e117a24dab0d27fdca85
Kathiresan M <kmkathirr10@gmail.com>
2026-01-25 21:41:02 +0530

    Implement logic to find 1st non-repeating element

--- Find_1st_Non_Repeating_Element.c
+++ Find_1st_Non_Repeating_Element.c
@@ -10,6 +10,23 @@ int main()
   for(i=0;i<size;i++)
     scanf("%d",&a[i]);
 
+ for(i=0;i<size;i++)
+    {
+      count=0;
+      
+      for(j=0;j<size;j++)
+        {
+          if(a[i] == a[j])
+            count++;
+        }
+      if(count==1){
+        printf("1st Non Repeating Element is : %d",a[i]);
+        found =1;
+      break;
+    }
+    }
+if(!found)
+  printf("1st Non Repeating Element not found");
   
 
   return 0; 
