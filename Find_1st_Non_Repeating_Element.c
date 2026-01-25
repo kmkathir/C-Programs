5de9094509dca2f739c6b575bcd56132d9c1e9e1
Kathiresan M <kmkathirr10@gmail.com>
2026-01-25 21:40:07 +0530

    Initialize array input for non-repeating element search
    Added basic structure for finding the first non-repeating element in an array.

--- Find_1st_Non_Repeating_Element.c
+++ Find_1st_Non_Repeating_Element.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int a[10],size,count,i,j,found;
 
+  printf("Enter Array Size: ");
+  scanf("%d",&size);
+
+  printf("Enter Array Elements : ");
+  for(i=0;i<size;i++)
+    scanf("%d",&a[i]);
+
+  
+
+  return 0; 
+}
