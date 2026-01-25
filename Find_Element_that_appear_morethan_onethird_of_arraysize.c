2f4f2c3bb0fcc81ae32126319c6bebbd1612350b
Kathiresan M <kmkathirr10@gmail.com>
2026-01-25 22:16:34 +0530

    Initialize program to find frequent array elements
    Added basic structure for finding elements that appear more than one-third of the array size.

--- Find_Element_that_appear_morethan_onethird_of_arraysize.c
+++ Find_Element_that_appear_morethan_onethird_of_arraysize.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ int a[10],i,j,count,found=0,size;
 
+  printf("Enter Array size : ");
+  scanf("%d",&size);
+
+  printf("Enter array elements : ");
+  for(i=0;i<size;i++)
+    scanf("%d",&a[i]);
+
+  printf("Elements that are more than n/3 times are :\n");
+
+
+  
+
+  return 0;
+}
