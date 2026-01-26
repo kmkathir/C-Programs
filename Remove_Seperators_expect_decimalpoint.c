ef6ed7285ca64dcd9d48a5293e43d6b9f0f2a470
Kathiresan M <kmkathirr10@gmail.com>
2026-01-26 22:18:12 +0530

    Update Remove_Seperators_expect_decimalpoint.c

--- Remove_Seperators_expect_decimalpoint.c
+++ Remove_Seperators_expect_decimalpoint.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  
+  char a[100];
+  scanf("%s",a);
+  for(int i=0;a[i]!='\0';i++){
+     if(a[i]>='0' && a[i]<= '9' || a[i]=='.'){
+        printf("%c",a[i]);
+     }
+  }
 
+return 0;
 }
