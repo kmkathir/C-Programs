7b101390523aef9a3d86713286dc56bbcfe1ac05
Kathiresan M <kmkathirr10@gmail.com>
2026-01-26 22:43:48 +0530

    Initialize C program for array input
    Add basic structure for a C program to input an array.

---
+++ 1st_Half_Ascending_2nd_Half_Descending.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a[100],i,j,temp,size;
+
+  printf("Enter array size : ");
+  scanf("%d",&size);
+
+  printf("Enter array elements : ");
+  for(i=0;i<size;i++)
+  scanf("%d",&a[i]);
+
+  
+return 0;
+}
