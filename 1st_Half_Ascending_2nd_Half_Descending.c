4b6560ae58d80beb6f6113e8f463377c4e60790d
Kathiresan M <kmkathirr10@gmail.com>
2026-01-26 22:44:14 +0530

    Sort first half of array in ascending order
    Implemented sorting for the first half of the array in ascending order.

--- 1st_Half_Ascending_2nd_Half_Descending.c
+++ 1st_Half_Ascending_2nd_Half_Descending.c
@@ -10,6 +10,20 @@ int main()
   for(i=0;i<size;i++)
   scanf("%d",&a[i]);
 
+  //1st half of array
+  
+  for(i=0;i<size/2;i++){
+    for(j=i+1;j<size/2;j++){
+      if(a[i] > a[j])
+      {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
+  
+
   
 return 0;
 }
