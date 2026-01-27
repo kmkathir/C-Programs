ca1384aa844c2214bff6b3f6762102153ed0df59
Kathiresan M <kmkathirr10@gmail.com>
2026-01-27 22:42:55 +0530

    Update and rename Array_having_+ive&-ive_Numbers_Alternatively to Array_having_+ive&-ive_Numbers_Alternatively.c

---
+++ Array_having_+ive&-ive_Numbers_Alternatively.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+  int a[100];
+
+  printf("Enter array size :");
+  scanf("%d",&size);
+
+  printf("Enter array elements :");
+  for(i=o;i<size;i++)
+    scanf("%d",&a[i]);
+
+
+
+
+  return 0;
+}
