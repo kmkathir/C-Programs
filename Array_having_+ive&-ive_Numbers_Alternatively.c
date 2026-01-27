106418352612010df4a335b6347ee8d13ef9a4ae
Kathiresan M <kmkathirr10@gmail.com>
2026-01-27 22:45:19 +0530

    Enhance array handling for positive and negative numbers
    Added arrays to store positive and negative numbers separately.

--- Array_having_+ive&-ive_Numbers_Alternatively.c
+++ Array_having_+ive&-ive_Numbers_Alternatively.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-  int a[100];
+  int a[100],p[100],n[100],i,j,size;
 
   printf("Enter array size :");
   scanf("%d",&size);
@@ -11,7 +11,16 @@ int main()
   for(i=o;i<size;i++)
     scanf("%d",&a[i]);
 
-
+  //seperate pos and neg elements
+  
+for(i=0;i<size;i++)
+  {
+    if(a[i] >= 0)
+      p[p++] = a[i];
+    else
+      n[n++] = a[i];
+  }
+  
 
 
   return 0;
