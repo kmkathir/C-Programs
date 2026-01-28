618404e60c58003e21d51520944e16c8e28e243e
Kathiresan M <kmkathirr10@gmail.com>
2026-01-28 18:24:03 +0530

    Implemented  code for comma formatting for digit groups
    Add commas after every three digits in the input string.

--- Adding_Comma_After_Every_3Digits.c
+++ Adding_Comma_After_Every_3Digits.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i,len;
+    
+    scanf("%s",a);
+    len=strlen(a);
+    
+    for(i=0;i<len;i++){
+    printf("%c",a[i]);
+    
+        if((len-i-1) % 3 ==0 && i!=len-1 )
+        {
+            printf(",");
+        }
+    }
 
+    return 0;
+}
