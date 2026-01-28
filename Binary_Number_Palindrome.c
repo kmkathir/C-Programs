b07096fe00a293672e6c91c56eb205b9bedac826
Kathiresan M <kmkathirr10@gmail.com>
2026-01-28 12:28:06 +0530

    Palindrome code successfully coded
    Fix palindrome checking logic and correct return statement.

--- Binary_Number_Palindrome.c
+++ Binary_Number_Palindrome.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char num[100];
+  int i,len;
+  scanf("%s",num);
+  len=strlen(num);
+  for(i=0;i<len-1;i++)
+    {
+    if(num[i] != num[len - i -1])
+    {
+      printf("Not A Palindrome");
+      break;
+    }
+  }
+  printf("Palindrome");
 
+  retrun 0;
+}
