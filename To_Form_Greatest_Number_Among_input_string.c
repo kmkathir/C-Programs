ad9f7faf97004b5d6b45eef80f821cf471c2d745
Kathiresan M <kmkathirr10@gmail.com>
2026-01-28 17:31:48 +0530

    Coded program to form greatest number from string
    Implement a program to form the greatest number from input string.

--- To_Form_Greatest_Number_Among_input_string.c
+++ To_Form_Greatest_Number_Among_input_string.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+  char a[100];
+  int i,j,temp,len;
+  
+  scanf("%s",a);
+  len=strlen(a);
+  
+  for(i=0;i<len-1;i++){
+      for(j=i+1;j<len;j++){
+          if(a[i] < a[j])
+          {
+              temp = a[i];
+              a[i] = a[j];
+              a[j] = temp;
+          }
+      }
+  }
+  
+  printf("%s",a);
+  
+  
+    return 0;
+}
