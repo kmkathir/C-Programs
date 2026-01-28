8b24321708f5197932a97bb8a2681eb6d9da0610
Kathiresan M <kmkathirr10@gmail.com>
2026-01-28 10:18:01 +0530

    Coded to find rightmost '1' in binary

--- Find_Position_of_Rightmost_Binary_Number.c
+++ Find_Position_of_Rightmost_Binary_Number.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+  char num[100];
+  int i,found=0,pos=1;
+
+ printf("Enter a binary number :");
+ scanf("%s",num);
+
+  for(i= strlen(num) -1;i>=0;i--){
+  if(num[i] == '1'){
+    printf("%d",pos);
+    found=1;
+    break;
+  }
+  pos++;
+  }
+  if(!found)
+    printf("-1");
+
+  return 0;
+    
+
+}
