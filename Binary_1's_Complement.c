7435c088657cf1ea424540e7e806694deeecfe34
Kathiresan M <kmkathirr10@gmail.com>
2026-01-28 12:45:01 +0530

    Add 1's complement calculation for binary input
    Implement a program to calculate the 1's complement of a binary number.

--- Binary_1's_Complement.c
+++ Binary_1's_Complement.c
@@ -1,1 +1,23 @@
 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char num[100];
+  int i,len;
+
+  scanf("%s",num);
+  len=strlen(num);
+  for(i=0;i<len;i++)
+    {
+      if(num[i] == '0'){
+        num[i] = '1';
+      }
+      else
+        num[i] = '0';
+    }
+printf("%s",num);
+
+  return 0;
+  
+}
