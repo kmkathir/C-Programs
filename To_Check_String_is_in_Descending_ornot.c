e81551fa16f47daef252e392d2e94764110d3c2f
Kathiresan M <kmkathirr10@gmail.com>
2026-01-28 18:04:37 +0530

    Implemented code for string descending order check
    Check if a string is in descending order and print result.

--- To_Check_String_is_in_Descending_ornot.c
+++ To_Check_String_is_in_Descending_ornot.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int i,len,found=0;
+    
+    scanf("%s",a);
+    len = strlen(a);
+    
+    for(i=0;i<len-1;i++)
+    {
+        if(a[i] < a[i+1]){
+            printf("No");
+            return 0;
+        }
+    }
+    
+    printf("Yes");
+
+    return 0;
+}
 
